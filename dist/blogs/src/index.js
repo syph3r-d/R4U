f09e7ed01eddc77c30fc0ef0944511c9b3bf01ab
Thulasithan Gnanenthiram <thulasithangnanenthiram@gmail.com>
2023-04-21 22:19:16 +0530

    First_Commit

---
+++ dist/blogs/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+    <p>Hello World</p>
+  </React.StrictMode>
+);
+
+
